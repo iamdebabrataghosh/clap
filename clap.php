e4206f3c2187d9d6fa040538b0377f555810e487
Debabrata Ghosh <31041415+iamdebabrataghosh@users.noreply.github.com>
2017-12-14 15:07:04 +0530

    Added plugin general information.

---
+++ clap.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Clap
+Plugin URI:   https://wordpress.org/plugins/
+Description:  WordPress plugin to allow end user clap on any content
+Version:      0.1
+Author:       Debabrata Ghosh
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  clap, wp-clap
+Domain Path:  /languages
+*/
